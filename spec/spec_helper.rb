5be03185b19a33961640866d8fa56b2ad07d9d6e
Toby Retallick <toby.retallick@gmail.com>
2018-05-14 16:51:14 +0100

    Remove Firefox browser warning - no longer required

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,24 +10,8 @@ Capybara.default_max_wait_time = 10
 RSpec.configure do |config|
   config.disable_monkey_patching!
   config.before(:all) do
-    check_firefox_version
     page.driver.browser.manage.window.resize_to(1920, 1080)
   end
-end
-
-def check_firefox_version
-  session = Capybara.current_session
-  driver = session.driver
-  version = driver.browser.capabilities.version
-  version_minor = version.split('.').take(2).join('.').to_f
-  puts firefox_warning(version_minor) if version_minor > 57.0
-end
-
-def firefox_warning(version)
-  <<-HEREDOC
-    Warning! Capybara is testing against Firefox version: #{version}.
-    Capybara may experience problems clicking buttons for any version greater than 57.0.X
-  HEREDOC
 end
 
 require_relative 'helpers/google_analytics_helper'
